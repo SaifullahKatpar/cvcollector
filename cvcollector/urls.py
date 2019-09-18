79cc9402db18586f038301f7e32e2a9f7dcee74d
SaifullahKatpar <saifullahkatpar@gmail.com>
2019-09-19 00:49:39 +0500

    added host

--- cvcollector/urls.py
+++ cvcollector/urls.py
@@ -1,4 +1,5 @@
-"""cvselector URL Configuration
+"""cvcollector
+ URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
